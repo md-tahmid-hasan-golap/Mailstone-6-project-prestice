a6db1ba06454c858cbb14eec3898d0c4590e015b
golap-raj <golapraj47@gmail.com>
2025-03-18 23:02:09 +0600

    add fildset

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+//   display none this id 
+document.getElementById("navbar_section").style.display = "none";
+document.getElementById("button_section").style.display = "none";
+document.getElementById("child_text").style.display = "none";
+document.getElementById("question_section").style.display = "none";
+
+
+// login id eventHandlar set
+
+document.getElementById("login_button").addEventListener("click", function () {
+alert()
+
+})
